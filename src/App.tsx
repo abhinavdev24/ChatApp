8f5532cce239610b92a5e5cc87111908767e4d97
Abhinav Kumar <1rn18cs002.abhinav@gmail.com>
2024-01-12 23:35:55 +0530

    feat: configure tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import "./style.scss";
 import { ReactNode, useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { AuthContextProvider } from "./context/AuthContext.tsx";
 import { ChatContextProvider } from "./context/ChatContext.tsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <AuthContextProvider>
